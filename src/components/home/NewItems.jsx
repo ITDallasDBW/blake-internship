0821268c6d189f5c0d479a5361b49302e7d89d0e
Blake <32065552+ITDallasDBW@users.noreply.github.com>
2026-02-08 16:15:09 -0600

    getItems component should be 100%. Need to work on countdown timer.

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -26,8 +26,8 @@ const NewItems = () => {
   //Axios API call
   async function getNewFetch() {
     const response = await axios.get(BASE_URL);
-    // setNewFetch(response.data);
-    // setLoading(false);
+    setNewFetch(response.data);
+    setLoading(false);
     console.log(response.data);
   }
 
@@ -124,131 +124,160 @@ const NewItems = () => {
                 ? //Render skeleton slides while loading
                   [...Array(keenSize || 4)].map((_, id) => (
                     <div className="keen-slider__slide" key={id}>
-                      <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12">
-                        <div className="nft__item">
-                          <div className="author_list_pp">
-                            <Link
-                              to="/"
-                              data-bs-toggle="tooltip"
-                              data-bs-placement="top"
-                              title="Creator: Monica Lucas"
-                            >
-                              <Skeleton
-                                width="50px"
-                                height="50px"
-                                borderRadius="50%"
-                              />
-                              <i className="fa fa-check"></i>
-                            </Link>
-                          </div>
-
-                          <div className="nft__item_wrap">
-                            <div className="nft__item_extra">
-                              <div className="nft__item_buttons">
-                                <button>Buy Now</button>
-                                <div className="nft__item_share">
-                                  <h4>Share</h4>
-                                  <a
-                                    href="https://www.facebook.com/sharer/sharer.php?u=https://gigaland.io"
-                                    target="_blank"
-                                    rel="noreferrer"
-                                  >
-                                    <i className="fa fa-facebook fa-lg"></i>
-                                  </a>
-                                  <a
-                                    href="https://twitter.com/intent/tweet?url=https://gigaland.io"
-                                    target="_blank"
-                                    rel="noreferrer"
-                                  >
-                                    <i className="fa fa-twitter fa-lg"></i>
-                                  </a>
-                                  <a href="mailto:?subject=I%20wanted%20you%20to%20see%20this%20site&body=Check%20out%20this%20site%20https://gigaland.io">
-                                    <i className="fa fa-envelope fa-lg"></i>
-                                  </a>
-                                </div>
-                              </div>
-                            </div>
-
-                            <Link to={``}>
-                              <Skeleton width="100%" height="350px" />
-                            </Link>
-                          </div>
-                          <div className="nft__item_info">
-                            <Link to={``}>
-                              <Skeleton width="180px" height="30px" />
-                            </Link>
-                            <Skeleton width="100px" height="20px" />
-                          </div>
-                          <div className="nft__item_like">
-                            <Skeleton width="30px" height="15px" />
-                          </div>
+                      <div className="nft__item">
+                        <div className="author_list_pp">
+                          <Link
+                            to="/"
+                            data-bs-toggle="tooltip"
+                            data-bs-placement="top"
+                            title="Creator: Monica Lucas"
+                          >
+                            <Skeleton
+                              width="50px"
+                              height="50px"
+                              borderRadius="50%"
+                            />
+                            <i className="fa fa-check"></i>
+                          </Link>
                         </div>
 
+                        <div className="nft__item_wrap">
+                          <div className="nft__item_extra">
+                            <div className="nft__item_buttons">
+                              <button>Buy Now</button>
+                              <div className="nft__item_share">
+                                <h4>Share</h4>
+                                <a
+                                  href="https://www.facebook.com/sharer/sharer.php?u=https://gigaland.io"
+                                  target="_blank"
+                                  rel="noreferrer"
+                                >
+                                  <i className="fa fa-facebook fa-lg"></i>
+                                </a>
+                                <a
+                                  href="https://twitter.com/intent/tweet?url=https://gigaland.io"
+                                  target="_blank"
+                                  rel="noreferrer"
+                                >
+                                  <i className="fa fa-twitter fa-lg"></i>
+                                </a>
+                                <a href="mailto:?subject=I%20wanted%20you%20to%20see%20this%20site&body=Check%20out%20this%20site%20https://gigaland.io">
+                                  <i className="fa fa-envelope fa-lg"></i>
+                                </a>
+                              </div>
+                            </div>
+                          </div>
+
+                          <Link to={``}>
+                            <Skeleton width="100%" height="350px" />
+                          </Link>
+                        </div>
+                        <div className="nft__item_info">
+                          <Link to={``}>
+                            <Skeleton width="180px" height="30px" />
+                          </Link>
+                          <Skeleton width="100px" height="20px" />
+                        </div>
+                        <div className="nft__item_like">
+                          <Skeleton width="30px" height="15px" />
+                        </div>
                       </div>
                     </div>
                   ))
                 : //Render actual data when loaded
-                  console.log("No")}
-            </div>
-          </div>
+                  newFetch.map((item, index) => (
+                    <div className="keen-slider__slide">
+                      <div className="nft__item" key={index}>
+                        <div className="author_list_pp">
+                          <Link
+                            to={`author/${item.authorId}`}
+                            data-bs-toggle="tooltip"
+                            data-bs-placement="top"
+                            title="Creator: Monica Lucas"
+                          >
+                            <img
+                              src={item.authorImage}
+                              alt=""
+                              className="lazy"
+                            />
+                            <i className="fa fa-check"></i>
+                          </Link>
+                        </div>
+                        {/* {item.expiryDate && (
+                          <CountDown expiryDate={item.expiryDate} />
+                        )} */}
+                        <div className="nft__item_wrap">
+                          <div className="nft__item_extra">
+                            <div className="nft__item_buttons">
+                              <button>Buy Now</button>
+                              <div className="nft__item_share">
+                                <h4>Share</h4>
+                                <a
+                                  href="https://www.facebook.com/sharer/sharer.php?u=https://gigaland.io"
+                                  target="_blank"
+                                  rel="noreferrer"
+                                >
+                                  <i className="fa fa-facebook fa-lg"></i>
+                                </a>
+                                <a
+                                  href="https://twitter.com/intent/tweet?url=https://gigaland.io"
+                                  target="_blank"
+                                  rel="noreferrer"
+                                >
+                                  <i className="fa fa-twitter fa-lg"></i>
+                                </a>
+                                <a href="mailto:?subject=I wanted you to see this site&amp;body=Check out this site https://gigaland.io">
+                                  <i className="fa fa-envelope fa-lg"></i>
+                                </a>
+                              </div>
+                            </div>
+                          </div>
 
-          {/* {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft__item">
-                <div className="author_list_pp">
-                  <Link
-                    to="/"
-                    data-bs-toggle="tooltip"
-                    data-bs-placement="top"
-                    title="Creator: Monica Lucas"
-                  >
-                    <img className="lazy" src={AuthorImage} alt="" />
-                    <i className="fa fa-check"></i>
-                  </Link>
-                </div>
-                <div className="de_countdown">5h 30m 32s</div>
-
-                <div className="nft__item_wrap">
-                  <div className="nft__item_extra">
-                    <div className="nft__item_buttons">
-                      <button>Buy Now</button>
-                      <div className="nft__item_share">
-                        <h4>Share</h4>
-                        <a href="#" target="_blank" rel="noreferrer">
-                          <i className="fa fa-facebook fa-lg"></i>
-                        </a>
-                        <a href="#" target="_blank" rel="noreferrer">
-                          <i className="fa fa-twitter fa-lg"></i>
-                        </a>
-                        <a href="#">
-                          <i className="fa fa-envelope fa-lg"></i>
-                        </a>
+                          <Link to={`/item-details/${item.nftId}`}>
+                            <img
+                              src={item.nftImage}
+                              alt=""
+                              className="lazy nft__item_preview"
+                            />
+                          </Link>
+                        </div>
+                        <div className="nft__item_info">
+                          <Link to={`/item-details/${item.nftId}`}>
+                          <h4>{item.title}</h4>
+                          </Link>
+                          <div className="nft__item_price">{item.price} ETH</div>
+                          <div className="nft__item_like">
+                            <i className="fa fa-heart"></i>
+                            <span>{item.likes}</span>
+                          </div>
+                        </div>
                       </div>
+                      
                     </div>
-                  </div>
-
-                  <Link to="/">
-                    <img
-                      src={nftImage}
-                      className="lazy nft__item_preview"
-                      alt=""
-                    />
-                  </Link>
-                </div>
-
-                <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <div className="nft__item_price">3.08 ETH</div>
-                  <div className="nft__item_like">
-                    <i className="fa fa-heart"></i>
-                    <span>69</span>
-                  </div>
-                </div>
-              </div>
+                  ))}
             </div>
-          ))} */}
+                        {instanceRef.current && (
+              <>
+                <div className="owl-nav">
+                  <button
+                    type="button"
+                    onClick={() => instanceRef.current?.prev()}
+                    className="owl-prev"
+                  >
+                    <span></span>
+                    {"<"}
+                  </button>
+                  <button
+                    type="button"
+                    className="owl-next"
+                    onClick={() => instanceRef.current?.next()}
+                  >
+                    <span>{">"}</span>
+                  </button>
+                </div>
+              </>
+            )}
+          </div>
         </div>
       </div>
     </section>
@@ -257,24 +286,3 @@ const NewItems = () => {
 
 export default NewItems;
 
-// {instanceRef.current && (
-//   <>
-//     <div className="owl-nav">
-//       <button
-//         type="button"
-//         onClick={() => instanceRef.current?.prev()}
-//         className="owl-prev"
-//       >
-//         <span></span>
-//         {"<"}
-//       </button>
-//       <button
-//         type="button"
-//         className="owl-next"
-//         onClick={() => instanceRef.current?.next()}
-//       >
-//         <span>{">"}</span>
-//       </button>
-//     </div>
-//   </>
-// )}
