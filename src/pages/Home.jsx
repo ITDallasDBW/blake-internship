1ba537483ad2897e4818f0da476258624f66d156
Blake <32065552+ITDallasDBW@users.noreply.github.com>
2026-03-04 11:16:38 -0600

    Animation added to home screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,21 @@ import LandingIntro from "../components/home/LandingIntro";
 import NewItems from "../components/home/NewItems";
 import TopSellers from "../components/home/TopSellers";
 
+//Animations
+//Fade up, Fade in.
+//Google 'animate on scroll' library
+//Add to divs you want to be fancy
+//Link in top right goes to github
+//Install details in ReadMe...npm install, etc
+//npm install --save aos@next
+//import AOS from 'aos';
+//import 'aos/dist/aos.css';
+//then run use effect with Aos.init() inside
+//Within div (outside classname) data-aos="fade-right" (or whatever)
+
+
+
+
 const Home = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
